5c7798ff1381007b25a8540c1662f6e11cb26802
이가현 <dlrkgus0858@gmail.com>
2025-12-19 00:34:20 +0900

    Delete stage2_fc_mp directory

--- stage2_fc_mp/tests/test_stage2_mp.py
+++
@@ -1,7 +0,0 @@
-from reasoner import KB
-
-
-def test_mp_chain():
-    kb = KB(facts={"P"}, rules=[("IMPLIES", "P", "Q"), ("IMPLIES", "Q", "R")])
-    kb.forward_chain()
-    assert "Q" in kb.facts and "R" in kb.facts
